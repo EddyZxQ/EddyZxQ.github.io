ba5ef4fcff3c31e93e8ce638548eb29b123f8943
Edgar Lopez <theskril70@gmail.com>
2021-07-01 17:04:35 +0200

    portafolio

---
+++ JS/btnDespliegue.js
@@ -0,0 +1,14 @@
+
+let btnDesplegar = document.getElementById("btnToggle");
+let caja = document.getElementById("desplegable");
+
+
+
+btnDesplegar.onclick = function (){
+
+caja.classList.toggle('active');
+btnDesplegar.classList.toggle('active');
+
+}
+
+
---
+++ JS/redireccionar.js
@@ -0,0 +1,14 @@
+
+
+
+let btnRedireccionarHotelSoler = document.getElementById("hotelSoler");
+let btnRedireccionarClinicaVeterinaria = document.getElementById("clinicaVeterinaria");
+
+
+btnRedireccionarHotelSoler.onclick = function (){
+    window.location.href = "https://eddyzxq.github.io/hotelsoler/";
+}
+
+btnRedireccionarClinicaVeterinaria.onclick = function (){
+    window.location.href = "http://www.w3schools.com";
+}
